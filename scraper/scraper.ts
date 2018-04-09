afd2fa87134a1f1fb5c0e1ceb7cad51bbc2b71e2
Brian Cui <brian.cui@live.com>
2018-04-09 16:44:28 -0500

    Treat newline breaks as different textnodes

--- scraper/scraper.ts
+++ scraper/scraper.ts
@@ -6,7 +6,10 @@ import Promise = require("bluebird");
 const dom: JSDOM = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
 
 function findTextNodes(root: HTMLElement): Array<String> {
-    let text_content: Array<String> = [root.textContent];
+    let text_content: Array<String> = 
+        root.textContent.split("\n")
+            .map((str: String) => { return str.trim() })
+            .filter((str: String) => { return str !== "" });
 
     /*
     if (root.children.length > 0) {
@@ -20,16 +23,22 @@ function findTextNodes(root: HTMLElement): Array<String> {
     return text_content;
 }
 
-request("https://www.reddit.com").then((res) => {
+request("https://www.buzzfeed.com/lol/").then((res) => {
     const dom: JSDOM = new JSDOM(res);
     const anchor_nodes: Array<HTMLAnchorElement> = Array.from(dom.window.document.querySelectorAll('a'));
 
     for (let i = 0; i < anchor_nodes.length; ++i) {
         let anchor_node = anchor_nodes[i];
-        let output: String = anchor_node.href;
-        if (!output) continue;
+        let dest_link: String = anchor_node.href.trim();
+ 
+        // skip destination links that are empty or start with javascript:
+        if (!dest_link || dest_link.indexOf("javascript:") === 0) continue;
+
+        // skip links that have no text content
+        let text_content: Array<String> = findTextNodes(anchor_node);
+        if (text_content.length === 0 || text_content.join("").trim() === "") continue;
         
-        console.log(output + " : " + JSON.stringify(findTextNodes(anchor_node)));
+        console.log(dest_link + " : " + JSON.stringify(text_content));
     }
 });
 //console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
