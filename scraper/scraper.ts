bc1ef7818eaffe0b7f149c5a5ff0698f0069cdae
Brian Cui <brian.cui@live.com>
2018-04-09 18:43:38 -0500

    Added read from file feature

--- scraper/scraper.ts
+++ scraper/scraper.ts
@@ -1,15 +1,26 @@
 /// <reference path="./node_modules/@types/jsdom/index.d.ts"/>
+import fs = require("fs");
 import { JSDOM } from "jsdom";
 import request = require("request-promise");
 import Promise = require("bluebird");
 
-const dom: JSDOM = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+const args: Array<string> = process.argv.slice(2);
+if (!args[0]) {
+    console.log("Usage: node scraper.js INPUTFILE");
+    process.exit(1);
+}
 
-function findTextNodes(root: HTMLElement): Array<String> {
-    let text_content: Array<String> = 
+// read each url line by line
+const input_file_lines = fs.readFileSync(args[0], "utf8")
+    .split("\n")
+    .map((str: string) => { return str.trim() })
+    .filter((str: string) => { return str !== "" });
+
+function findTextNodes(root: HTMLElement): Array<string> {
+    let text_content: Array<string> = 
         root.textContent.split("\n")
-            .map((str: String) => { return str.trim() })
-            .filter((str: String) => { return str !== "" });
+            .map((str: string) => { return str.trim() })
+            .filter((str: string) => { return str !== "" });
 
     /*
     if (root.children.length > 0) {
@@ -23,22 +34,25 @@ function findTextNodes(root: HTMLElement): Array<String> {
     return text_content;
 }
 
-request("https://www.buzzfeed.com/lol/").then((res) => {
-    const dom: JSDOM = new JSDOM(res);
-    const anchor_nodes: Array<HTMLAnchorElement> = Array.from(dom.window.document.querySelectorAll('a'));
-
-    for (let i = 0; i < anchor_nodes.length; ++i) {
-        let anchor_node = anchor_nodes[i];
-        let dest_link: String = anchor_node.href.trim();
- 
-        // skip destination links that are empty or start with javascript:
-        if (!dest_link || dest_link.indexOf("javascript:") === 0) continue;
-
-        // skip links that have no text content
-        let text_content: Array<String> = findTextNodes(anchor_node);
-        if (text_content.length === 0 || text_content.join("").trim() === "") continue;
-        
-        console.log(dest_link + " : " + JSON.stringify(text_content));
-    }
-});
-//console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
+// retrieve each page and prints its links, asynchronously
+input_file_lines.forEach((url) => {
+    request("https://www." + url).then((res) => {
+        // render the HTML, then retrieve all the anchor tags
+        const dom: JSDOM = new JSDOM(res);
+        const anchor_nodes: Array<HTMLAnchorElement> = Array.from(dom.window.document.querySelectorAll('a'));
+    
+        for (let i = 0; i < anchor_nodes.length; ++i) {
+            let anchor_node = anchor_nodes[i];
+            let dest_link: string = anchor_node.href.trim();
+     
+            // skip destination links that are empty or start with javascript:
+            if (!dest_link || dest_link.indexOf("javascript:") === 0) continue;
+    
+            // skip links that have no text content
+            let text_content: Array<string> = findTextNodes(anchor_node);
+            if (text_content.length === 0 || text_content.join("").trim() === "") continue;
+            
+            console.log(dest_link + " : " + JSON.stringify(text_content));
+        }
+    });
+})
